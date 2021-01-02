866382cb28243b0a7c9edf0d1d8627e7b9f24c86
edward <eduardcp20@gmail.com>
2021-01-02 13:21:16 -0400

    sabado 02 enero

--- resources/views/partials/navigations/admin.blade.php
+++
@@ -1,4 +0,0 @@
-<li><a class="nav-link" href="#">{{ __("Administrar cursos") }}</a></li>
-<li><a class="nav-link" href="#">{{ __("Administrar estudiantes") }}</a></li>
-<li><a class="nav-link" href="#">{{ __("Administrar profesores") }}</a></li>
-@include('partials.navigations.logged')
--- resources/views/partials/navigations/student.blade.php
+++
@@ -1,5 +0,0 @@
-<li><a class="nav-link" href="#">{{ __("Mi perfil") }}</a></li>
-<li><a class="nav-link" href="#">{{ __("Mis suscripciones") }}</a></li>
-<li><a class="nav-link" href="#">{{ __("Mis facturas") }}</a></li>
-<li><a class="nav-link" href="#">{{ __("Mis cursos") }}</a></li>
-@include('partials.navigations.logged')
