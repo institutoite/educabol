09dd54da2794198da3d15cc6db876bb59516c318
edward <eduardcp20@gmail.com>
2021-09-06 11:33:53 -0400

    05/09/2021
    Dar estilos a la vista de inscritos

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -1,8 +1,38 @@
 <div class="mt-8">
     <div class="max-w-7xl mx-auto px-4 grid grid-cols-3 gap-8">
         <div class="col-span-2">
-            {!!$current->iframe!!}
-            {{$current->name}}
+            <div class="embed-responsive">
+                {!!$current->iframe!!}
+            </div>
+
+            <h1 class="text-3xl text-gray-600 font-bold mt-4">
+                {{$current->name}}
+            </h1>
+
+            @if ($current->description)
+                <div class="text-gray-600">
+                    {{$current->description->name}}
+                </div>
+            @endif
+
+            <div class="flex items-center mt-4 cursor-pointer">
+                <i class="fas fa-toggle-off text-2xl text-gray-600"></i>
+                <p class="text-sm ml-2">Marcar esta unidad como culminada</p>
+            </div>
+
+            <div class="card mt-2">
+                <div class="card-body flex text-gray-500 font-bold">
+                    
+                    @if ($this->previous)
+                        <a class="cursor-pointer">Tema anterior</a>
+                    @endif
+
+                    @if ($this->next)
+                        <a class="ml-auto cursor-pointer">Siguiente tema</a>
+                    @endif
+                </div>
+            </div>
+
             <p>Indice: {{$this->index}}</p>
             <p>Previus: @if ($this->previous)
                 {{$this->previous->id}}
