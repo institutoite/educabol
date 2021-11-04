a655706be8aa12ebb7d2d3dbe4613822d1bb32ab
edward <eduardcp20@gmail.com>
2021-11-03 22:14:34 -0400

    03/11/2021
    Vista de niveles de los cursos

---
+++ resources/views/admin/levels/edit.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'Educabol')
+
+@section('content_header')
+    <h1>Lista de niveles</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+
+            </table>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -6,15 +6,15 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\LevelController;
 
 Route::get('', [HomeController::class, 'index'])->name('home');
 
 Route::resource('roles', RoleController::class)->names('roles');
 
-
 Route::resource('categories', CategoryController::class)->names('categories');
 
-
+Route::resource('levels', LevelController::class)->names('levels');
 
 Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
 
